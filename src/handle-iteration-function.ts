0249f482aee75d4b3ef4c35c1705f2c9c257a6e9
webiodigital <157547007+webiodigital@users.noreply.github.com>
2024-01-26 22:35:23 +0100

    Update handle-iteration-function.ts

--- src/handle-iteration-function.ts
+++ src/handle-iteration-function.ts
@@ -97,20 +97,9 @@ export default async ({ data, iterationInput, iterationIndex, stats, originalInp
     // Add images to state
     try {
         let imageIndex = 13;
-        let obrIndex = 1;
         data.forEach((item: any, itemIndex: number) => {
             if (item.skipDownload) return; // we skip item with this field
             let imagesFromPath = objectPath.get(item, pathToImageUrls);
-            // **************************************************
-            // **************************************************
-            // **************************************************
-            imagesFromPath.forEach((image:any, obrIndex: number) => {
-                obrIndex++;
-                console.log(image, obrIndex);
-            });
-            // **************************************************
-            // **************************************************
-            // **************************************************
             if (!Array.isArray(imagesFromPath) && typeof imagesFromPath !== 'string') {
                 stats.inc(props.itemsWithoutImages, updateStats);
                 return;
@@ -122,6 +111,15 @@ export default async ({ data, iterationInput, iterationIndex, stats, originalInp
                 stats.inc(props.itemsWithoutImages, updateStats);
                 return;
             }
+            // **************************************************
+            // **************************************************
+            // **************************************************
+            imagesFromPath.forEach((image:any, obrIndex: number) => {
+                console.log(image, obrIndex);
+            });
+            // **************************************************
+            // **************************************************
+            // **************************************************            
             imagesFromPath.forEach((image: any) => {
                 stats.inc(props.imagesTotal, updateStats);
                 if (typeof image !== 'string') {
